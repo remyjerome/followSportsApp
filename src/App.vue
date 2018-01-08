b4ac53c6ace4c871927a8bbed694992e4f3c43d4
Remy JEROME <r.jerome@FIDUCIAL.DOM>
2018-01-08 12:00:11 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <google-youtube>
+  </google-youtube>
+</template>
+
+<script>
+import GoogleYoutube from './google-youtube.vue'
+
+export default {
+  name: 'app',
+  components: {
+    GoogleYoutube
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/TableauDeBord.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <div class="container-tdb">
+    <h2>Tableau De Bord</h2>
+    <evolution-mensuration></evolution-mensuration>
+    <carnet-musculation></carnet-musculation>
+    <programtion-entrainement></programtion-entrainement>
+    <scheduler></scheduler>
+  </div>
+</template>
+
+<script>
+import EvolutionMensuration from './EvolutionMensuration.vue'
+import CarnetMusculation from './CarnetMusculation.vue'
+import ProgramtionEntrainement from './ProgramtionEntrainement.vue'
+import Scheduler from './Scheduler.vue'
+export default {
+  name: 'TableauDeBord',
+  components : {
+    EvolutionMensuration,
+    CarnetMusculation,
+    ProgramtionEntrainement,
+    Scheduler
+  },
+}
+</script>
+
+<style lang="css">
+</style>
